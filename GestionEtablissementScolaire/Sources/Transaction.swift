16f7ddf1b38aea02b3919472880aec2b69d274ef
hatfitzo <djedlyfitzgerome@gmail.com>
2025-10-15 21:36:34 -0400

    Add Transaction class for financial operations

--- GestionEtablissementScolaire/Sources/Transaction.swift
+++ GestionEtablissementScolaire/Sources/Transaction.swift
@@ -1,1 +1,31 @@
+//
+//  Economat.swift
+//  GestionEtablissementScolaire
+//
+//  Created by Djedly-Fitz GEROME on 09/10/25.
+//
+
+import Foundation
+
+class Transaction {
+    let id: UUID
+    let type: String
+    let montant: Double
+    let description: String
+    let date: Date
+    
+    init(type: String, montant: Double, description: String) {
+        self.id = UUID()
+        self.type = type
+        self.montant = montant
+        self.description = description
+        self.date = Date()
+    }
+    
+    func afficher() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy HH:mm"
+        print("\(type.uppercased()) - \(montant) HTG - \(description) - \(formatter.string(from: date))")
+    }
+}
 
